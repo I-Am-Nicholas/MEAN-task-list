02b5089459d7a8c2e0a961572162a578bf25e908
Nicholas Leacock <nicholas@swissmail.com>
2017-10-15 17:40:56 +0100

    Add integration test for interaction between server, router and static fileby testing for index.html title.
    Changed views folder to public folder.

--- controllers/index.js
+++ controllers/index.js
@@ -5,3 +5,6 @@ const render = require('./render.js')
 router.get('/', render);
 
 module.exports = router;
+
+// /* when the route is to the home page, get the value
+//  of the index property in the render constant/file. */
