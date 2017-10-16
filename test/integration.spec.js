830c139605647466be21517c4162e9673b056ecf
Nicholas Leacock <nicholas@swissmail.com>
2017-10-16 14:01:40 +0100

    Add integration test for retrieving correct item from db.

--- test/integration.spec.js
+++ test/integration.spec.js
@@ -38,6 +38,8 @@ describe("Index Page", () => {
 
 describe("Tasks Page", () => {
 
+let task2 = {"_id":"59e4547d566c36829f9b22ac","Task 2":"Take Mark VII for test flight.","isDone":false}
+
   it('responds with 200 for route /api/tasks', (done) => {
     request(server)
       .get('/api/tasks')
@@ -57,8 +59,20 @@ describe("Tasks Page", () => {
     });
   });
 
-});
+  it('should return correct db item', (done) => {
+    request(server)
+      .get('/api/tasks')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if(err) throw err;
+        expect(res.body[1].Task2).to.equal('Take Mark VII for test flight.', done)
+        expect(res.body[1].isDone).to.equal(false);
+        done();
+      });
+  });
 
+});
 
 it('Invalid route returns correct status code', (done) => {
   request(server)
