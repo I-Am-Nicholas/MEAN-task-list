e64bbae1e9d264ceeb65dbab7478ef8ee3402982
Nicholas Leacock <nicholas@swissmail.com>
2017-11-08 16:27:48 +0000

    Refactor delete method extacting findOne code and wrapping it in its own function.
    Remove surplus returns throughout task.js.

--- models/tasks.js
+++ models/tasks.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
 
-const db = require('../db');
+const db = require('../db')
 
 //SET SCHEMA
-const schema = require('../public/schemas/schema');
-const thisSchema = new mongoose.Schema(schema);
+const schema = require('../public/schemas/schema')
+const thisSchema = new mongoose.Schema(schema)
 
 //DEFINE MODEL
 if (typeof Task === 'undefined') {
@@ -14,36 +14,36 @@ if (typeof Task === 'undefined') {
 
 exports.allTasks = async function(res) {
   await db.connect();
-  let findTasks = Task.find((err, tasks) => {
+  return Task.find((err, tasks) => {
     err ? res.send(err) : res.send(tasks);
-    return tasks;
-  });
-  return findTasks;
-};
+  })
+}
 
 exports.oneTask = async function(req, res) {
-  await db.connect();
+  await db.connect()
   let getParamId = req.params.id
-  await Task.findOne( {_id: getParamId} )
-  .exec( (err, task) => {
-    err ? console.log('Custom err msg from oneTask'+err) : res.send(task);
-    return task
-  });
-};
+  await Task.findOne( {_id: getParamId}, async (err, task) => {
+    if (err) {console.log('Error in findOne: '+ err)}
+    await res.send(task)
+  })
+}
 
 exports.deleteTask = async function(req, res) {
   await db.connect();
   let getParamId = req.params.id
-  await Task.findOneAndRemove( {'_id' : getParamId}, async (err, task) => {
+  await Task.findOneAndRemove( {_id : getParamId}, async (err, task) => {
     if(err){console.log("Custom findOneAndRemove err msg: "+err)}
     if(task === null){res.send("Unable to find task: "+getParamId)}
     else {
-      let t = await Task.find((err, tasks) => {
-        err ? console.log('Custom deleteTask err msg: '+err) : tasks
-      })
-      await res.send("Database now contains "+t.length+" tasks.")
+      await dbSize(res)
     }
   })
+}
 
-
+let dbSize = (res) => {
+  return Task.find((err, tasks) => {
+    err ? console.log('Custom deleteTask err msg: '+err) : tasks
+  }).then(async tasks => {
+    await res.send("Database now contains "+tasks.length+" tasks.")
+  })
 }
