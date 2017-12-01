26478927545cd440afb93e9cffbfd718891984b5
Nicholas Leacock <nicholas@swissmail.com>
2017-12-01 09:20:20 +0200

    Add e2e test for prsence of task list in DOM.

--- client/task-list/e2e/app.e2e-spec.ts
+++ client/task-list/e2e/app.e2e-spec.ts
@@ -1,12 +1,19 @@
-
-describe('task-list App', () => {
+describe('Task-List App', () => {
 
   beforeEach(() => {
     browser.get("http://localhost:3000/");
   });
 
-  it('should display welcome message', () => {
-    expect(browser.getTitle()).toEqual('TaskList');
-  });
-  
+  let taskList = element(by.tagName("ul"));
+
+  describe('Home Page',() => {
+
+    it('should display the app\'s title', () => {
+      expect(browser.getTitle()).toEqual('Task List');
+      expect(taskList.isPresent()).toBe(true);
+    });
+
+  })
+
+
 });
